5de14f3f1b30282b743c03e57142fd5fc8cbde17
GiBeom Park <rlqja530@gmail.com>
2024-07-31 13:59:10 +0900

    add GAS sensor

--- 아두이노/main/main.ino
+++ 아두이노/main/main.ino
@@ -7,26 +7,24 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include "dht11.h"
-#include "oled_u8g2.h"
 #define DHT11PIN D2 // D2 포트에 DHT11 센서를 연결해야 함
 
 const char* ssid = "Ccom";      // WiFi SSID
 const char* password = "ccom0414";  // WiFi 비밀번호
 dht11 DHT11; // 온습도 센서
-OLED_U8G2 oled;
 int light_sensor = A1; // 조도센서
+int gasSensorPin = A4; // MQ-2 센서의 아날로그 출력 핀
 
 struct SensorData {
   int temperature;
   int humidity;
   int lightLevel;
+  int gas;
 };
 
 void setup() {
-  Serial.begin(115200);
-  oled.setup();
+  Serial.begin(9600);
   setupWiFi();
-
 }
 
 void loop() {  
@@ -34,9 +32,8 @@ void loop() {
 
 
   serialPrint(data); //시리얼 모니터에 출력 표시
-  oledPrint(data); //oled 모듈에 출력 표시
 
-  const char* postServerName = "http://192.168.81.192:8080/"; //URL 수정
+  const char* postServerName = "http://192.168.137.51:5010/sensor"; //URL 수정
   String jsonPayload = createJsonPayload(data);
   httpPostRequest(postServerName, jsonPayload);
   
@@ -51,18 +48,12 @@ void serialPrint(SensorData data){
   Serial.print(data.humidity); // 습도 값 출력
   Serial.print("%, Light Level: ");
   Serial.print(data.lightLevel); // 조도 값 출력
-  Serial.println("lux");
+  Serial.println(" lux");
+  delay(100);
+  Serial.print("  gas: ");
+  Serial.println(analogRead(gasSensorPin)); // 가스 값 출력
 }
 
-void oledPrint(SensorData data){
-  // OLED 디스플레이에 출력
-  //oled.setLine(1, "Sensor Data"); // OLED 모듈 1번째 줄에 저장
-  oled.setLine(1, "Temp: " + String(data.temperature) + "C"); // OLED 모듈 1번째 줄에 온도 값 저장
-  oled.setLine(2, "Humi: " + String(data.humidity) + "%"); // OLED 모듈 2번째 줄에 습도 값 저장
-  oled.setLine(3, "Light: " + String(data.lightLevel) + "lux"); // OLED 모듈 3번째 줄에 조도 값 저장
-  oled.display(); // OLED 모듈 출력
-
-}
 // DHT11 및 조도센서 값을 읽고 SensorData 구조체로 반환하는 함수
 SensorData readSensors() {
   DHT11.read(DHT11PIN); // 온습도 센서(DTH11) 값 측정
@@ -71,7 +62,7 @@ SensorData readSensors() {
   data.temperature = DHT11.temperature;
   data.humidity = DHT11.humidity;
   data.lightLevel = analogRead(light_sensor); // 조도 값 측정
-
+  data.gas=analogRead(gasSensorPin);
   return data;
 }
 
@@ -113,7 +104,8 @@ String createJsonPayload(SensorData data) {
   String jsonPayload = "{";
   jsonPayload += "\"Temperature\":\"" + String(data.temperature) + "\",";
   jsonPayload += "\"humidity\":\"" + String(data.humidity) + "\",";
-  jsonPayload += "\"lightLevel\":\"" + String(data.lightLevel) + "\"";
+  jsonPayload += "\"lightLevel\":\"" + String(data.lightLevel) + "\",";
+  jsonPayload += "\"gas\":\"" + String(data.gas) + "\"";
   jsonPayload += "}";
 
   Serial.println("JSON Payload: " + jsonPayload); // 디버그를 위해 JSON 출력
